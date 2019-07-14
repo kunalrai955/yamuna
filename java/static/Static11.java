6562622808ef3485a68ff1fcbb0acaf506ae4069
kavitha Rayi <kunalrai955@gmail.com>
2019-07-14 11:17:58 +0530

    my first commit

---
+++ java/static/Static11.java
@@ -0,0 +1,16 @@
+class Static11
+{
+	static int a;
+	static void m1(int a){
+//static11.a by default has zero and replace 30 with 0
+		a=Static11.a;
+System.out.println(a);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println(a);
+		m1(30);
+		System.out.println(a);
+	}
+}
+//Here 30 replaced with 0 as static variable by default 0
